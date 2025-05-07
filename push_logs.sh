98119f70545a5bdebc7d9535d2f64c4980d675e3
Big JIm <bj474006@gmail.com>
2025-05-07 12:04:39 +0800

    Update push_logs.sh

--- push_logs.sh
+++ push_logs.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
 cd /Users/Shared/system532/stringCup
 
-date_str=$(date +%d%m)
-git config --local user.email "you@example.com"
-git config --local user.name "Your Name"
+# Set Git user info (customize these)
+git config --local user.email "Test_account@example.com"
+git config --local user.name "Tester"
 
-git mv liquid/log.txt "liquid/${date_str}_log.txt"
-git commit -m "Rename and update log for $date_str"
+# Remove the existing 'liquid' directory from the repo
+git rm -r liquid
+
+# Copy the local 'liquid' folder into the repo (replace with your source path if different)
+cp -r /Users/Shared/system532/stringCup/liquid /Users/Shared/system532/stringCup/
+
+# Stage all changes (additions and deletions)
+git add .
+
+# Commit and push
+git commit -m "Replace liquid directory with latest version from device"
 git push https://<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>@github.com/BigJ1m/stringCup.git main
